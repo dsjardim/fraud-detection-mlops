76f22b2fb39aae79c36c4b9e44012589d29a0279
DiegoJardim <diego.jardim@poatek.com>
2021-10-11 12:23:40 -0300

    experimenting the QuadraticDiscriminantAnalysis classifier

--- src/train.py
+++ src/train.py
@@ -6,7 +6,6 @@ import pandas as pd
 import numpy as np
 
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
