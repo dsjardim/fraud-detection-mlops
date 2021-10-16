809c1ac5ba9750d1b7d043d1c357d390d0998789
DiegoJardim <diego.jardim@poatek.com>
2021-10-15 22:05:57 -0300

    trying pipeline

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -3,9 +3,5 @@ import pathlib as pl
 import os
 
 
-def test_model_creation1():
-    assert pl.Path("/home/runner/work/fraud-detection-mlops/fraud-detection-mlops/data/model.pickle").is_file()
-
-
-def test_model_creation2():
-    assert pl.Path("/home/runner/work/fraud-detection-mlops/data/model.pickle").is_file()
+def test_model_creation():
+    assert pl.Path(__file__).parent.joinpath("data", "model.pickle").is_file()
