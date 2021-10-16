a7c83be571e37f81ee0329ecd702b48ad7854693
DiegoJardim <diego.jardim@poatek.com>
2021-10-16 18:20:33 -0300

    trying API stuff

--- src/app.py
+++ src/app.py
@@ -14,7 +14,8 @@ app = FastAPI(title='Credit Card Fraud Detection API')
 
 @app.get("/")
 def home():
-    return "Congratulations! Your API is working as expected. Now head over to http://localhost:8000/docs."
+    return "Congratulations! Your API is working as expected. Now head over to " \
+           "https://credit-fraud-detection-mlops.herokuapp.com. "
 
 
 @app.post("/predict/")
@@ -42,7 +43,8 @@ def predict(csv_file: UploadFile = File(...)):
 nest_asyncio.apply()
 
 # Host depends on the setup you selected (docker or virtual env)
-host = "0.0.0.0" if os.getenv("DOCKER-SETUP") else "127.0.0.1"
+# host = "0.0.0.0" if os.getenv("DOCKER-SETUP") else "127.0.0.1"
+host = "https://credit-fraud-detection-mlops.herokuapp.com/"
 
 # Spin up the server!
 uvicorn.run(app, host=host, port=8000)
