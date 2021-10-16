ecfb45ee44cda5926ca14dcaa314225f89c9f421
DiegoJardim <diego.jardim@poatek.com>
2021-10-15 22:11:36 -0300

    trying tests

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -5,3 +5,7 @@ import os
 
 def test_model_creation():
     assert pl.Path(__file__).parent.joinpath("data", "model.pickle").is_file()
+
+
+def test_model_creation1():
+    assert pl.Path(__file__).joinpath("data", "model.pickle").is_file()
